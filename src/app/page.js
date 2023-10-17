49f1f1a25c2a7ee0df75954f94e79f683e154a80
AnthonySaDev <anthonysareis11@gmail.com>
2023-10-17 11:02:08 -0300

    animacoes

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Head from 'next/head';
+import { motion } from "framer-motion";
 import Image from 'next/image';
 import pessoal from '../../public/pessoal.jpg';
 import about from '../../public/about.jpg';
@@ -18,19 +19,55 @@ export default function Home() {
       </Head>
       <Header />
       <main className='w-full text-[#00303F]'>
-        <div className='text-center lg:flex items-center justify-center lg:h-screen pt-24 lg:pt-0'>
+        <motion.div
+          className='text-center lg:flex items-center justify-center lg:h-screen pt-24 lg:pt-0'
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
           <div className='flex flex-col gap-10 '>
-            <h1 className='text-[2rem] font-bold'>
+            <motion.h1
+              className='text-[2rem] font-bold'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.5, duration: 1 }}
+            >
               Bem-vindo ao Lar das Velhinhas  Semeando Amor!
-            </h1>
-            <p className='lg:p-6 p-2 lg:w-7/12 mx-auto text-[1.2rem]'>
-              Nosso Lar é um refúgio de carinho, cuidado e dignidade para as pessoas mais sábias e experientes de nossa sociedade. No Lar das Velhinhas, estamos comprometidos em proporcionar um ambiente acolhedor e seguro para nossos residentes idosos. No entanto, para continuarmos a oferecer essa qualidade de vida, contamos com a sua ajuda.</p>
-            <a href='/#ajude'>
-              <button className='bg-[#DCAE1D] hover:brightness-125 transition-all ease-in duration-300 p-4 rounded-lg w-fit mx-auto'>AJUDAR AGORA</button>
-            </a>
+            </motion.h1>
+            <motion.p
+              className='lg:p-6 p-2 lg:w-7/12 mx-auto text-[1.2rem]'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1, duration: 1 }}
+            >
+              Nosso Lar é um refúgio de carinho, cuidado e dignidade para as pessoas mais sábias e experientes de nossa sociedade. No Lar das Velhinhas, estamos comprometidos em proporcionar um ambiente acolhedor e seguro para nossos residentes idosos. No entanto, para continuarmos a oferecer essa qualidade de vida, contamos com a sua ajuda.
+            </motion.p>
+            <motion.a
+              href='/#ajude'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.5, duration: 1 }}
+            >
+              <motion.button
+                className='bg-[#DCAE1D] hover:brightness-125 transition-all ease-in duration-300 p-4 rounded-lg w-fit mx-auto'
+              >
+                AJUDAR AGORA
+              </motion.button>
+            </motion.a>
           </div>
-          <Image src={pessoal} alt="pessoal" className='lg:w-7/12 mx-auto h-fit mt-5 object-cover lg:rounded-l-[4rem] ' />
-        </div>
+          <motion.div   
+          className='lg:w-7/12 mx-auto h-fit mt-5 '   
+          initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}>
+          <Image
+            src={pessoal}
+            alt="pessoal"
+           className='object-cover lg:rounded-l-[4rem]'
+          />
+          </motion.div>
+        </motion.div>
+
 
         <div className='my-10 text-center' id='sobre'>
           <h1 className='text-[2rem] font-bold'>
@@ -47,29 +84,29 @@ export default function Home() {
 
         <div className='my-10 text-center' id='ajude'>
 
-        <h1 className='text-[2rem] font-bold'>
+          <h1 className='text-[2rem] font-bold'>
             Como Você Pode Ajudar:
           </h1>
           <p className='lg:p-6 p-2 my-4 lg:w-7/12 mx-auto text-[1.2rem]'>
             Estamos sempre em busca de doações para garantir que possamos oferecer os melhores cuidados possíveis aos nossos residentes. Você pode ajudar de várias maneiras:</p>
-            <div className='flex flex-col items-start lg:w-1/2 lg:mx-auto justify-center gap-4 px-3 text-left text-[1rem]'>
+          <div className='flex flex-col items-start lg:w-1/2 lg:mx-auto justify-center gap-4 px-3 text-left text-[1rem]'>
             <p>
-            <strong>Doação financeira:</strong> Qualquer quantia faz a diferença. Sua contribuição ajuda a cobrir despesas com alimentação, cuidados médicos e atividades recreativas.<br></br>
-            <strong>💜 Doe pelo Pix 16.923.245/0001-20</strong>
+              <strong>Doação financeira:</strong> Qualquer quantia faz a diferença. Sua contribuição ajuda a cobrir despesas com alimentação, cuidados médicos e atividades recreativas.<br></br>
+              <strong>💜 Doe pelo Pix 16.923.245/0001-20</strong>
             </p>
-       
+
             <p>
 
-            <strong>Doação de tempo:</strong> Se você puder dedicar seu tempo como voluntário, será uma dádiva inestimável para nossos idosos. Seja parte de sua jornada e compartilhe seu carinho.<br></br>
-            <strong>💚 Aceitamos visitas no endereço: Rua Dom João Pimenta, 65 - Centro, Montes Claros - MG, 39400-003</strong>
-          </p>
-          <p>
-             <strong>Doação de suprimentos:</strong> Aceitamos doações de alimentos, roupas, produtos de higiene e outros itens essenciais.<br></br>
-             <strong>🧡 Para mais informações entre em contato:(38) 3221-1299</strong>
-          </p>
-            </div>
-            <Image src={doe} alt='doe' className='rounded-full object-cover lg:w-4/12 mx-auto mt-5' />
-          
+              <strong>Doação de tempo:</strong> Se você puder dedicar seu tempo como voluntário, será uma dádiva inestimável para nossos idosos. Seja parte de sua jornada e compartilhe seu carinho.<br></br>
+              <strong>💚 Aceitamos visitas no endereço: Rua Dom João Pimenta, 65 - Centro, Montes Claros - MG, 39400-003</strong>
+            </p>
+            <p>
+              <strong>Doação de suprimentos:</strong> Aceitamos doações de alimentos, roupas, produtos de higiene e outros itens essenciais.<br></br>
+              <strong>🧡 Para mais informações entre em contato:(38) 3221-1299</strong>
+            </p>
+          </div>
+          <Image src={doe} alt='doe' className='rounded-full object-cover lg:w-4/12 mx-auto mt-5' />
+
         </div>
 
       </main>
